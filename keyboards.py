a5fccfa04c63d51b297bea3e0e3883c49d650d63
DonSparrow1996 <vorobej.den@gmail.com>
2026-03-07 17:30:07 +0200

    add date keyboard

--- keyboards.py
+++ keyboards.py
@@ -3,7 +3,8 @@ from config import (
     BUTTON_BOOK,
     BUTTON_CANCEL,
     SERVICES,
-    AVAILABLE_TIMES
+    AVAILABLE_TIMES,
+    AVAILABLE_DATES
 )
 
 
@@ -23,6 +24,23 @@ def get_service_keyboard():
         resize_keyboard=True
     )
 
+def get_date_keyboard():
+    keyboard = []
+
+    for i in range(0, len(AVAILABLE_DATES), 2):
+        row = [KeyboardButton(text=AVAILABLE_DATES[i])]
+
+        if i + 1 < len(AVAILABLE_DATES):
+            row.append(KeyboardButton(text=AVAILABLE_DATES[i + 1]))
+
+        keyboard.append(row)
+
+    keyboard.append([KeyboardButton(text=BUTTON_CANCEL)])
+
+    return ReplyKeyboardMarkup(
+        keyboard=keyboard,
+        resize_keyboard=True
+    )
 
 def get_time_keyboard():
     keyboard = []
